ceddbb0a8345b24728ca338d150cad85d544e774
candle1503 <61449197+candle1503@users.noreply.github.com>
2020-04-08 09:40:24 +0900

    Set pointController

---
+++ src/com/coupang/point/PointController.java
@@ -0,0 +1,55 @@
+package com.coupang.point;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PointController
+ */
+@WebServlet("/PointController")
+public class PointController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PointController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//한글 Encoding 처리
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		//pathInfo
+		String command = request.getPathInfo();
+		
+		//Method 형식
+		String method = request.getMethod();
+		
+		//Forward(true), redirect(false) 선택
+		boolean check = true;
+		
+		//path를 담을 변수
+		String path ="";
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
